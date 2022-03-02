b9fc1f68fec19de6949bafb97f0826dbe45d9e7c
snehanaik4737 <97469677+snehanaik4737@users.noreply.github.com>
2022-03-02 23:00:05 +0530

    done with the assignment

---
+++ main.js
@@ -0,0 +1,14 @@
+const add= require("./add");
+
+//add(1,3); 
+console.log(add(1,3));
+
+
+const subtract= require("./subtract");
+ console.log(subtract(5,3));
+
+ const multiply= require("./multiply");
+ console.log(multiply(5,3));
+
+ const divide= require("./divide");
+ console.log(divide(10,5));
