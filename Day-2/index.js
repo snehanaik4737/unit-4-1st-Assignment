05b71e85408f0054c10739484fa1530fde74f9df
snehanaik4737 <97469677+snehanaik4737@users.noreply.github.com>
2022-03-04 20:47:58 +0530

    day-2-Assignment

--- Day-2/index.js
+++ Day-2/index.js
@@ -1,1 +1,19 @@
 
+const express = require("express");
+
+const app = express();
+//console.log(app);
+
+
+app.get("",function(req,res){
+res.send("Hello");
+})
+
+
+app.get("/books", function(req,res){
+    res.send({book1:"Harry potter", book2:"Gitanjali",book3:"Sapiens",book4:"Pride and Prejudice"})
+});
+
+app.listen(4001,()=>{
+    console.log("Listening on port 4000")
+})
